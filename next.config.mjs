068b1b9073fad8f9789307f178e3a20ce6a76bea
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-02-27 21:57:24 +0300

    toujours dans l'intégartion de la page d'accueil

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,16 @@
 const nextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: 3000,
+        pathname: "/**"
+      }
+    ]
+  }
 };
 
 export default nextConfig;
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,35 +2,35 @@ import "@/styles/globals.css";
 import '@/styles/home.css'
 import { AuthProvider } from "@/contexts/AuthContext";
 import { UnheadProvider, createHead } from '@unhead/react/client'
-import { useHead } from "@unhead/react";
+import { useHead, useSeoMeta } from "@unhead/react";
+
+const head = createHead()
+
+function GlobalSEO() {
+  useHead({
+    meta: [{ name: "robots", content: "index, follow" }]
+  });
+
+  useSeoMeta({
+    ogSiteName: "LUMINI School",
+    ogLocale: "fr_MG",
+    ogType: "website",
+    ogImage: `${process.env.NEXT_PUBLIC_APP_BASE_URL}/images/lumini-school-logo.png`,
+    twitterCard: "summary_large_image",
+    twitterImage: `${process.env.NEXT_PUBLIC_APP_BASE_URL}/images/lumini-school-logo.png`
+  });
+
+  return null;
+}
 
 export default function App({ Component, pageProps }) {
 
-  const head = createHead()
-
-  useHead({
-    meta: [
-      { name: 'robots', content: 'index, follow' }
-    ]
-  })
-
-  useSeoMeta({
-
-    ogSiteName: 'LUMINI School',
-    ogLocale: 'fr_MG',
-    ogType: 'website',
-    ogImage: `${process.env.NEXT_PUBLIC_APP_BASE_URL}/images/lumini-school-logo.png`,
-
-    twitterCard: 'summary_large_image',
-    twitterImage: `${process.env.NEXT_PUBLIC_APP_BASE_URL}/images/lumini-school-logo.png`
-
-  })
-
   return (
     <UnheadProvider head={head}>
-      <AuthProvider>
-        <Component {...pageProps} />
-      </AuthProvider>
+        <GlobalSEO/>
+        <AuthProvider>
+          <Component {...pageProps} />
+        </AuthProvider>
     </UnheadProvider>
   );
 }
