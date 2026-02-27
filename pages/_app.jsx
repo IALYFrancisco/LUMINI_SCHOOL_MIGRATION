e61514c90c1462d2e4ac48edee5f721fbc773524
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-02-27 21:20:12 +0300

    configuration globale de unhead

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,17 @@
 import "@/styles/globals.css";
 import '@/styles/home.css'
 import { AuthProvider } from "@/contexts/AuthContext";
+import { UnheadProvider, createHead } from '@unhead/react/client'
 
 export default function App({ Component, pageProps }) {
+
+  const head = createHead()
+
   return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+    <UnheadProvider head={head}>
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </UnheadProvider>
   );
 }
