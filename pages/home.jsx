00b42826869df37eae894ac93d94b7e02c5047ee
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-02-27 21:12:06 +0300

    ajout du section formations dans la page d'accueil

--- pages/home.jsx
+++ pages/home.jsx
@@ -1,9 +1,11 @@
 import { Header } from "@/components/header"
+import { Formations } from "@/components/formations"
 
 export function Home(){
     return (
         <>
             <Header></Header>
+            <Formations></Formations>
         </>
     )
 }
