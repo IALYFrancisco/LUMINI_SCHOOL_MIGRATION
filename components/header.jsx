8d973b2056c5c8d27cbe0affa6345ba26d0dca1f
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-02-27 15:30:01 +0300

    composant header développé

--- components/header.jsx
+++ components/header.jsx
@@ -1,2 +1,34 @@
 import { Nav } from "./nav";
 import Link from "next/link";
+import Image from "next/image";
+import arrow from '@/public/images/arrow.png'
+import femme from '@/public/images/laptop3.png'
+
+export function Header(){
+    return(
+        <>
+            <Nav></Nav>
+            <header>
+                <div>
+                    <div className="left">
+                        <h1>L'informatique vous intéresse ? Voulez-vous y former ?</h1>
+                        <p>Vous êtes donc au bon endroit 👍. Plusieurs modules de nos formations sont faites pour vous.</p>
+                        <div className="actions">
+                            <a href="#demo-formations">
+                                <button>En savoir plus <Image src={arrow} alt="flèche pointant  à droite"/></button>
+                            </a>
+                            <Link href="/formations">
+                                <button>Voir toutes les formations</button>
+                            </Link>
+                        </div>
+                    </div>
+                    <div className="right">
+                        <div className="img-container">
+                            <Image src={femme} className="femme" alt="jeune femme tenant un laptop"/>
+                        </div>
+                    </div>
+                </div>
+            </header>
+        </>
+    )
+}
