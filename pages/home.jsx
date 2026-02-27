734620425ecc39181ab4be438368dccfdefd6990
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-02-27 21:30:24 +0300

    ajout des métadonnées SEO dans la page d'accueil

--- pages/home.jsx
+++ pages/home.jsx
@@ -1,7 +1,26 @@
 import { Header } from "@/components/header"
 import { Formations } from "@/components/formations"
+import { useHead, useSeoMeta } from "@unhead/react"
 
 export function Home(){
+
+    useHead({
+        link: [
+            { rel: 'canonical', href: 'https://luminischool.onrender.com' }
+        ]
+    })
+
+    useSeoMeta({
+
+        title: 'Accueil | LUMINI School - Plateforme de formation en informatique',
+
+        ogTitle: 'Accueil | LUMINI School - Plateforme de formation en informatique',
+        ogUrl: 'https://luminischool.onrender.com',
+
+        twitterTitle: 'Accueil | LUMINI School - Plateforme de formation en informatique'
+
+    })
+
     return (
         <>
             <Header></Header>
