688581934e88057e6aa7969b13a7b17a234bf3d9
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-02-27 21:25:05 +0300

    ajout des balises SEO globales

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,10 +2,29 @@ import "@/styles/globals.css";
 import '@/styles/home.css'
 import { AuthProvider } from "@/contexts/AuthContext";
 import { UnheadProvider, createHead } from '@unhead/react/client'
+import { useHead } from "@unhead/react";
 
 export default function App({ Component, pageProps }) {
 
   const head = createHead()
+
+  useHead({
+    meta: [
+      { name: 'robots', content: 'index, follow' }
+    ]
+  })
+
+  useSeoMeta({
+
+    ogSiteName: 'LUMINI School',
+    ogLocale: 'fr_MG',
+    ogType: 'website',
+    ogImage: 'https://raw.githubusercontent.com/IALYFrancisco/ASSETS/refs/heads/main/LUMINI%20School/IMAGES/lumini-school-logo.png',
+
+    twitterCard: 'summary_large_image',
+    twitterImage: 'https://raw.githubusercontent.com/IALYFrancisco/ASSETS/refs/heads/main/LUMINI%20School/IMAGES/lumini-school-logo.png'
+
+  })
 
   return (
     <UnheadProvider head={head}>
