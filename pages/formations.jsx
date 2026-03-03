9567448d349c1c20bc55857057dd647481a9e0b1
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-03-03 21:23:10 +0300

    développement de la page liste des formations publiées

--- pages/formations.jsx
+++ pages/formations.jsx
@@ -1,4 +1,5 @@
-import Nav from "../components/nav"
+/* eslint-disable react/no-unescaped-entities */
+import { Nav } from '@/components/nav'
 import '../../public/styles/formationsPage.css'
 import { useEffect, useState } from "react"
 import axios from "axios"
