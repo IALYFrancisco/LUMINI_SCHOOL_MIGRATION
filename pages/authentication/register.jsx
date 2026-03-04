1447c9736d7ad38561aea91d1b19f844809b3122
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-03-04 09:49:20 +0300

    ajout de isNotAuthenticated dans la page register

--- pages/authentication/register.jsx
+++ pages/authentication/register.jsx
@@ -7,6 +7,7 @@ import { useState } from "react"
 import Head from "next/head"
 import Image from "next/image"
 import Link from "next/link"
+import IsNotAuthenticated from "@/components/isNotAuthenticated"
 
 export default function Register(){
 
@@ -44,49 +45,51 @@ export default function Register(){
     }
 
     return(
-        <>
-            <Head>
-                <title>Création de compte | LUMINI School - Plateforme de formation en informatique</title>
-                <link rel="canonical" href="https://luminischool.onrender.com/authentication/register" />
-                <meta name="description" content="Créez votre compte LUMINI School et bénéficiez d'un espace personnel complet pour gérer vos inscriptions, paiements et toutes vos activités sur la plateforme en toute simplicité."/>
-                
-                <meta property="og:title" content="Création de compte | LUMINI School - Plateforme de formation en informatique" />
-                <meta property="og:description" content="Créez votre compte LUMINI School et bénéficiez d'un espace personnel complet pour gérer vos inscriptions, paiements et toutes vos activités sur la plateforme en toute simplicité."/>
-                <meta property="og:url" content="https://luminischool.onrender.com/authentication/register" />
+        <IsNotAuthenticated>
+            <>
+                <Head>
+                    <title>Création de compte | LUMINI School - Plateforme de formation en informatique</title>
+                    <link rel="canonical" href="https://luminischool.onrender.com/authentication/register" />
+                    <meta name="description" content="Créez votre compte LUMINI School et bénéficiez d'un espace personnel complet pour gérer vos inscriptions, paiements et toutes vos activités sur la plateforme en toute simplicité."/>
+                    
+                    <meta property="og:title" content="Création de compte | LUMINI School - Plateforme de formation en informatique" />
+                    <meta property="og:description" content="Créez votre compte LUMINI School et bénéficiez d'un espace personnel complet pour gérer vos inscriptions, paiements et toutes vos activités sur la plateforme en toute simplicité."/>
+                    <meta property="og:url" content="https://luminischool.onrender.com/authentication/register" />
 
-                <meta name="twitter:title" content="Création de compte | LUMINI School - Plateforme de formation en informatique" />
-                <meta name="twitter:description" content="Créez votre compte LUMINI School et bénéficiez d'un espace personnel complet pour gérer vos inscriptions, paiements et toutes vos activités sur la plateforme en toute simplicité." />
-            </Head>
-            <Nav></Nav>
-            <section className="login-form">
-                <h2>Création de compte</h2>
-                <h5>à LUMINI School</h5>
-                <form onSubmit={handleSubmit(_handleSubmit)}>
-                    <Image src="/images/note.png" alt="note" className="laptop-mouse" width={400} height={400} priority />
-                    <Image src="/images/clavier (2).png" alt="clavier" className="mouse" width={375} height={375} priority />
-                    <div className="element">
-                        <label htmlFor="user-name">Votre nom complet :</label>
-                        <input type="text" id="user-name" placeholder="Ex: John Doe" { ...register('name', { required: true }) } required />
-                    </div>
-                    <div className="element">
-                        <label htmlFor="user-email">Votre adresse email :</label>
-                        <input type="email" id="user-email" placeholder="Ex: johndoe@example.com" { ...register('email', { required: true }) } required />
-                    </div>
-                    <div className="element">
-                        <label htmlFor="user-password">Votre mot de passe :</label>
-                        <input type="password" id="user-password" placeholder="Choisissez un mot de passe sécurisé" { ...register('password', { required: true }) } required />
-                    </div>
-                    <div className="element">
-                        <button disabled={registerLoading}>
-                            Soumettre
-                            { registerLoading && <Image src="/images/spinner.png" alt="loading spinner" width={50} height={50} priority />}
-                        </button>
-                    </div>
-                </form>
-                <span>
-                    <p>Vous avez déjà un compte? <Link href="/authentication/login">Se connecter</Link>.</p>
-                </span>
-            </section>
-        </>
+                    <meta name="twitter:title" content="Création de compte | LUMINI School - Plateforme de formation en informatique" />
+                    <meta name="twitter:description" content="Créez votre compte LUMINI School et bénéficiez d'un espace personnel complet pour gérer vos inscriptions, paiements et toutes vos activités sur la plateforme en toute simplicité." />
+                </Head>
+                <Nav></Nav>
+                <section className="login-form">
+                    <h2>Création de compte</h2>
+                    <h5>à LUMINI School</h5>
+                    <form onSubmit={handleSubmit(_handleSubmit)}>
+                        <Image src="/images/note.png" alt="note" className="laptop-mouse" width={400} height={400} priority />
+                        <Image src="/images/clavier (2).png" alt="clavier" className="mouse" width={375} height={375} priority />
+                        <div className="element">
+                            <label htmlFor="user-name">Votre nom complet :</label>
+                            <input type="text" id="user-name" placeholder="Ex: John Doe" { ...register('name', { required: true }) } required />
+                        </div>
+                        <div className="element">
+                            <label htmlFor="user-email">Votre adresse email :</label>
+                            <input type="email" id="user-email" placeholder="Ex: johndoe@example.com" { ...register('email', { required: true }) } required />
+                        </div>
+                        <div className="element">
+                            <label htmlFor="user-password">Votre mot de passe :</label>
+                            <input type="password" id="user-password" placeholder="Choisissez un mot de passe sécurisé" { ...register('password', { required: true }) } required />
+                        </div>
+                        <div className="element">
+                            <button disabled={registerLoading}>
+                                Soumettre
+                                { registerLoading && <Image src="/images/spinner.png" alt="loading spinner" width={50} height={50} priority />}
+                            </button>
+                        </div>
+                    </form>
+                    <span>
+                        <p>Vous avez déjà un compte? <Link href="/authentication/login">Se connecter</Link>.</p>
+                    </span>
+                </section>
+            </>
+        </IsNotAuthenticated>
     )
 }
