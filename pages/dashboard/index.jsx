1217e38b28d634f5a286ba1b9a8a2c55688fdb9e
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-03-06 10:18:05 +0300

    injéction du formation-list dans le projet

--- pages/dashboard/index.jsx
+++ pages/dashboard/index.jsx
@@ -1,9 +1,10 @@
+import FormationsList from "@/components/dashboard/formations/formations-list";
 import Dashboard from "@/components/layouts/dashboardLayout";
 
 export default function Index(){
     return(
         <Dashboard>
-            
+            <FormationsList/>
         </Dashboard>
     )
 }
