03b47fb714296026412134fece531c6a50c00fbe
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-03-06 07:54:22 +0300

    injéction du style du dashboard dans le projet

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -4,6 +4,7 @@ import '@/styles/formationsPage.css';
 import '@/styles/login.css';
 import '@/styles/registrations.css';
 import '@/styles/articleView.css';
+import '@/styles/dashboard.css';
 import "react-quill-new/dist/quill.snow.css";
 import { AuthProvider } from "@/contexts/AuthContext";
 import Head from "next/head"
