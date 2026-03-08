bbff184e43932492423df9b7261ebbf86b14cbb8
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-03-08 16:34:42 +0300

    changement du contenu du isAuthenticated

--- components/isAuthenticated.jsx
+++ components/isAuthenticated.jsx
@@ -1,6 +1,6 @@
 // This component allows to protect routes by not authenticated users
 
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import { useRouter } from "next/router";
 import { useAuth } from "@/contexts/AuthContext";
 import Loading from "@/components/loading";
@@ -9,21 +9,17 @@ import { toast } from "sonner";
 export default function IsAuthenticated({ children }) {
   const { user, loading } = useAuth();
   const router = useRouter();
-  const toastShown = useRef(false);
 
   useEffect(() => {
     if (!loading && !user) {
-      if (!toastShown.current) {
-        toast.info("Vous devez d'abord vous connecter à votre compte.");
-        toastShown.current = true;
-      }
+      toast.info("Vous devez d'abord vous connecter à votre compte.");
       router.replace("/authentication/login");
     }
   }, [user, loading, router]);
 
   if (loading) return <Loading />;
 
-  if (!user) return null;
+  if (!user) return <Loading />;
 
   return children;
 }
