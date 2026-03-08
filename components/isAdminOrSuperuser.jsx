6060a91bd8b39afe77680a17f9252c64dab3b0bc
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-03-08 21:07:46 +0300

    corréction du contenu de isAdminOrSuperuser.jsx

--- components/isAdminOrSuperuser.jsx
+++ components/isAdminOrSuperuser.jsx
@@ -1,15 +1,13 @@
 import { useRouter } from "next/router";
 import { useAuth } from "@/contexts/AuthContext";
 import Loading from "@/components/loading";
-import { useEffect } from "react";
 
 export default function IsAdminOrSuperuser({children}) {
-    const { user, loading } = useAuth()
     const router = useRouter()
+    const { user, loading } = useAuth()
 
-    useEffect(()=>{
-        if(!loading && user && (user.status === "admin" || user.status === "superuser")) return children
-    }, [children, loading, user])
     if(loading) return <Loading/>
+    if(user && (user.status === "admin" || user.status === "superuser")) return children
     return router.replace("/dashboard/inscriptions")
+
 }
--- components/layouts/formationLayout.jsx
+++ components/layouts/formationLayout.jsx
@@ -1,13 +1,14 @@
 import Head from "next/head"
+import IsAdminOrSuperuser from "../isAdminOrSuperuser"
 
 export default function FormationLayout({children}){
     return(
-        <>
+        <IsAdminOrSuperuser>
             <Head>
                 <title>Formations - Dashboard | LUMINI School</title>
             </Head>
             <h2>Formations</h2>
             {children}
-        </>
+        </IsAdminOrSuperuser>
     )
 }
