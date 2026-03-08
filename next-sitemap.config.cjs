05194dfd7d531d449b6621c96e031ce7e9e2797c
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-03-08 14:27:18 +0300

    amélioration encore du configuration du génération de robots et sitemap

--- next-sitemap.config.cjs
+++ next-sitemap.config.cjs
@@ -3,8 +3,10 @@ module.exports = {
     siteUrl: process.env.NEXT_PUBLIC_APP_BASE_URL,
     generateRobotsTxt: true,
     generateIndexSitemap: false,
+    outDir: "./dist",
     sitemapSize: 7000,
     changefreq: 'weekly',
     priority: 0.7,
+    autoLastmod: true,
     exclude: [ '/dashboard/*' ]
 }
