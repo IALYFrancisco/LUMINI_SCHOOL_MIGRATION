c1a07f03a7f3029d20832b61f50b6cfc51a3c266
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-03-08 21:26:54 +0300

    corréction de la page de création de formation

--- pages/dashboard/formation/create.jsx
+++ pages/dashboard/formation/create.jsx
@@ -4,6 +4,8 @@ import { useForm } from "react-hook-form"
 import axios from "axios"
 import { toast } from "sonner"
 import Image from "next/image"
+import Dashboard from "@/components/layouts/dashboardLayout"
+import FormationLayout from "@/components/layouts/formationLayout"
 
 export default function AddFormation(){
     
@@ -57,75 +59,79 @@ export default function AddFormation(){
     }
 
     return(
-        <>
-            <section className="add-formation-form">
-                <h3>Ajout d'une formation :</h3>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <div className="fieldsets-top">
-                        <fieldset>
-                            <div className="element">
-                                <label>Titre de la formation :</label>
-                                <input type="text" required placeholder="Ajoutez un titre pour la formation" { ...register("title", { required: true })}/>
+        <Dashboard>
+            <FormationLayout>
+                <>
+                    <section className="add-formation-form">
+                        <h3>Ajout d'une formation :</h3>
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <div className="fieldsets-top">
+                                <fieldset>
+                                    <div className="element">
+                                        <label>Titre de la formation :</label>
+                                        <input type="text" required placeholder="Ajoutez un titre pour la formation" { ...register("title", { required: true })}/>
+                                    </div>
+                                    <div className="element">
+                                        <label>Image de mis en avant pour la formation :</label>
+                                        <input disabled={ urlIsDefined } type="file" id="" required accept="image/jpeg, image/png" onChange={(e) => setImage(e.target.files[0])}/>
+                                        <input disabled={ imageIsDefined } type="url" id="" { ...register("url") } placeholder="Utilisez cet champ pour une image en ligne" />
+                                    </div>
+                                    <div className="element">
+                                        <label>Les prérequis du formation :</label>
+                                        <input type="text" id="" placeholder="Doivent être séparés par un point-virgule" { ...register("prerequisites", {required: true}) } required />
+                                    </div>
+                                </fieldset>
+                                <fieldset>
+                                    <div className="element">
+                                        <label>Descriptions de la formation : <p>nombre de mots : {wordCount} / 150</p></label>
+                                        <textarea cols="30" rows="10" required placeholder="Redigez ici les descriptions ..." { ...register("description", { required: "La description est obligatoire.", validate: {
+                                            minWords: (value) => 
+                                                value.trim().split(/\s+/).length >= 50 ||
+                                            "La description doit contenir au moins 50 mots.",
+                                            maxWords: (value) =>
+                                                value.trim().split(/\s+/).length <= 150 ||
+                                            "La description ne doit pas dépasser 150 mots."
+                                        } }) } ></textarea>
+                                    </div>
+                                    { errors.description && (
+                                        <p className="message">{errors.description.message}</p>
+                                    ) }
+                                </fieldset>
                             </div>
-                            <div className="element">
-                                <label>Image de mis en avant pour la formation :</label>
-                                <input disabled={ urlIsDefined } type="file" id="" required accept="image/jpeg, image/png" onChange={(e) => setImage(e.target.files[0])}/>
-                                <input disabled={ imageIsDefined } type="url" id="" { ...register("url") } placeholder="Utilisez cet champ pour une image en ligne" />
+                            <div className="fieldsets-bottom">
+                                <fieldset>
+                                    <div className="element">
+                                        <label>Date et heure de début du formation :</label>
+                                        <input type="datetime-local" required { ...register("beginDate", { required: true })}/>
+                                    </div>
+                                    <div className="element">
+                                        <label>Date et heure de fin du formation :</label>
+                                        <input type="datetime-local" required { ...register("endDate", { required: true }) }/>
+                                    </div>
+                                </fieldset>
+                                <fieldset>
+                                    <div className="element">
+                                        <label>Lieu du formation :</label>
+                                        <input type="text" required { ...register("coursePlace", { required: true })} placeholder="L'endroit où se déroule la formation"/>
+                                    </div>
+                                    <div className="element">
+                                        <label>Coût de la formation (en Ar) :</label>
+                                        <input type="number" min="1" required { ...register("coursePrice", { required: true }) } placeholder="Entrez un montant en ariary"/>
+                                    </div>
+                                </fieldset>
+                                <fieldset>
+                                    <div className="element">
+                                        <button disabled={addFormationLoading}>
+                                            Soumettre
+                                            { addFormationLoading && <Image src="/images/spinner.png" alt="loader spiner" width={50} height={50} priority />}    
+                                        </button>
+                                    </div>
+                                </fieldset>
                             </div>
-                            <div className="element">
-                                <label>Les prérequis du formation :</label>
-                                <input type="text" id="" placeholder="Doivent être séparés par un point-virgule" { ...register("prerequisites", {required: true}) } required />
-                            </div>
-                        </fieldset>
-                        <fieldset>
-                            <div className="element">
-                                <label>Descriptions de la formation : <p>nombre de mots : {wordCount} / 150</p></label>
-                                <textarea cols="30" rows="10" required placeholder="Redigez ici les descriptions ..." { ...register("description", { required: "La description est obligatoire.", validate: {
-                                    minWords: (value) => 
-                                        value.trim().split(/\s+/).length >= 50 ||
-                                    "La description doit contenir au moins 50 mots.",
-                                    maxWords: (value) =>
-                                        value.trim().split(/\s+/).length <= 150 ||
-                                    "La description ne doit pas dépasser 150 mots."
-                                } }) } ></textarea>
-                            </div>
-                            { errors.description && (
-                                <p className="message">{errors.description.message}</p>
-                            ) }
-                        </fieldset>
-                    </div>
-                    <div className="fieldsets-bottom">
-                        <fieldset>
-                            <div className="element">
-                                <label>Date et heure de début du formation :</label>
-                                <input type="datetime-local" required { ...register("beginDate", { required: true })}/>
-                            </div>
-                            <div className="element">
-                                <label>Date et heure de fin du formation :</label>
-                                <input type="datetime-local" required { ...register("endDate", { required: true }) }/>
-                            </div>
-                        </fieldset>
-                        <fieldset>
-                            <div className="element">
-                                <label>Lieu du formation :</label>
-                                <input type="text" required { ...register("coursePlace", { required: true })} placeholder="L'endroit où se déroule la formation"/>
-                            </div>
-                            <div className="element">
-                                <label>Coût de la formation (en Ar) :</label>
-                                <input type="number" min="1" required { ...register("coursePrice", { required: true }) } placeholder="Entrez un montant en ariary"/>
-                            </div>
-                        </fieldset>
-                        <fieldset>
-                            <div className="element">
-                                <button disabled={addFormationLoading}>
-                                    Soumettre
-                                    { addFormationLoading && <Image src="/images/spinner.png" alt="loader spiner" width={50} height={50} priority />}    
-                                </button>
-                            </div>
-                        </fieldset>
-                    </div>
-                </form>
-            </section>
-        </>
+                        </form>
+                    </section>
+                </>
+            </FormationLayout>
+        </Dashboard>
     )
 }
