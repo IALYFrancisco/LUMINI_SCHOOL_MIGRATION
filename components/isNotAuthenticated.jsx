dba5bce15fa5ad39683c9ea231df88707dffb758
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-03-08 16:27:05 +0300

    corréction du non réactivité du composant isNotAuthenticated

--- components/isNotAuthenticated.jsx
+++ components/isNotAuthenticated.jsx
@@ -1,4 +1,4 @@
-This component allows to protect routes by authenticated users
+// This component allows to protect routes by authenticated users
 
 import { useEffect } from "react";
 import { useRouter } from "next/router";
@@ -15,33 +15,11 @@ export default function IsNotAuthenticated({ children }) {
     }
   }, [user, loading, router]);
 
+  // Pendant la vérification de l'auth
   if (loading) return <Loading />;
 
-  if (user) return null;
+  // Si user existe, on bloque l'affichage
+  if (user) return <Loading />;
 
   return children;
 }
-
-// import { useEffect } from "react";
-// import { useRouter } from "next/router";
-// import { useAuth } from "@/contexts/AuthContext";
-// import Loading from "@/components/loading";
-
-// export default function IsNotAuthenticated({ children }) {
-//   const router = useRouter();
-//   const { user, loading } = useAuth();
-
-//   useEffect(() => {
-//     if (!loading && user) {
-//       router.replace("/dashboard");
-//     }
-//   }, [user, loading, router]);
-
-//   // Pendant la vérification de l'auth
-//   if (loading) return <Loading />;
-
-//   // Si user existe, on bloque l'affichage
-//   if (user) return <Loading />;
-
-//   return children;
-// }
