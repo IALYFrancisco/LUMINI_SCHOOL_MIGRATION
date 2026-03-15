c9b70636d478de19eda056bb7a51b48352e138f7
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-03-15 15:51:02 +0300

    ajout des routes d'article dans le sitemap

--- next-sitemap.config.cjs
+++ next-sitemap.config.cjs
@@ -1,7 +1,20 @@
+import axios from 'axios'
+
 /** @type { import('next-sitemap').IConfig } */
 module.exports = {
     siteUrl: process.env.NEXT_PUBLIC_APP_BASE_URL,
     generateRobotsTxt: true,
+    additionalPaths: async ()=>{
+        try{
+            let articles = await axios.get(`${process.env.NEXT_PUBLIC_API_BASE_URL}/article/get`)
+            return articles.map( a => ({
+                loc: `/article/${a.slug}/`,
+                changefreq: "weekly",
+                priority: 0.8,
+                lastmod: 
+            }))
+        }catch{}
+    },
     generateIndexSitemap: false,
     outDir: "./dist",
     sitemapSize: 7000,
