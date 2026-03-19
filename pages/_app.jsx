7eabfd97e3ec71faf889412595b73b0aa1435fd6
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-03-19 14:16:29 +0300

    injéction du style user.css dans le projet

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -4,12 +4,13 @@ import '@/styles/login.css';
 import '@/styles/formationsPage.css';
 import '@/styles/dashboard/dashboard.css';
 import '@/styles/dashboard/inscription.css';
-import '@/styles/registrations.css';
-import '@/styles/articleView.css';
-import '@/styles/notFound.css';
-import '@/styles/dashboard/formation.css';
 import '@/styles/dashboard/article.css';
 import "react-quill-new/dist/quill.snow.css";
+import '@/styles/dashboard/formation.css';
+import '@/styles/registrations.css';
+import '@/styles/user.css';
+import '@/styles/articleView.css';
+import '@/styles/notFound.css';
 import { AuthProvider } from "@/contexts/AuthContext";
 import Head from "next/head"
 import { Toaster } from "sonner"
