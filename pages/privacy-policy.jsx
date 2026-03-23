44f4427eabcdb995823aa22f232050a86cea24c8
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-03-23 16:29:03 +0300

    ajout d'en-têtes pour la page des politiques de confidentialité

--- pages/privacy-policy.jsx
+++ pages/privacy-policy.jsx
@@ -6,16 +6,16 @@ export default function TermsAndConditions(){
     return (
         <>
             <Head>
-                <title>Conditions générales d’utilisation et de vente | LUMINI School - Plateforme de formation en informatique</title>
-                <link rel="canonical" href="https://luminischool.onrender.com/terms-and-conditions" />
-                <meta name="description" content="Consultez les conditions générales d’utilisation et de vente de LUMINI School : inscription, paiement, accès aux formations en présentiel et responsabilités."/>
+                <title>Politique de confidentialité | LUMINI School - Plateforme de formation en informatique</title>
+                <link rel="canonical" href="https://luminischool.onrender.com/privacy-policy" />
+                <meta name="description" content="Découvrez comment LUMINI School collecte, utilise et protège vos données personnelles, ainsi que la gestion des cookies et des paiements."/>
 
-                <meta property="og:title" content="Conditions générales d’utilisation et de vente | LUMINI School - Plateforme de formation en informatique" />
-                <meta property="og:description" content="Consultez les conditions générales d’utilisation et de vente de LUMINI School : inscription, paiement, accès aux formations en présentiel et responsabilités." key="og:description"/>
-                <meta property="og:url" content="https://luminischool.onrender.com/terms-and-conditions" />
+                <meta property="og:title" content="Politique de confidentialité | LUMINI School - Plateforme de formation en informatique" />
+                <meta property="og:description" content="Découvrez comment LUMINI School collecte, utilise et protège vos données personnelles, ainsi que la gestion des cookies et des paiements." key="og:description"/>
+                <meta property="og:url" content="https://luminischool.onrender.com/privacy-policy" />
 
-                <meta name="twitter:title" content="Conditions générales d’utilisation et de vente | LUMINI School - Plateforme de formation en informatique" />
-                <meta name="twitter:description" content="Consultez les conditions générales d’utilisation et de vente de LUMINI School : inscription, paiement, accès aux formations en présentiel et responsabilités." />       
+                <meta name="twitter:title" content="Politique de confidentialité | LUMINI School - Plateforme de formation en informatique" />
+                <meta name="twitter:description" content="Découvrez comment LUMINI School collecte, utilise et protège vos données personnelles, ainsi que la gestion des cookies et des paiements." />       
             </Head>
 
             <Nav/>
