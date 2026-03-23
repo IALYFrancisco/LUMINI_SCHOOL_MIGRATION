c65821247db1ff0514e04cb8245994be020b9f19
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-03-23 14:42:37 +0300

    ajout des en-têtes pour le SEO de la page

--- pages/terms-and-conditions.jsx
+++ pages/terms-and-conditions.jsx
@@ -1,4 +1,20 @@
-/* eslint-disable react/no-unescaped-entities */
+import Head from "next/head"
+
 export default function TermsAndConditions(){
-    return <h1>Conditions d'utilisation</h1>
+    return (
+        <>
+            <Head>
+                <title>Conditions générales d’utilisation et de vente | LUMINI School - Plateforme de formation en informatique</title>
+                <link rel="canonical" href="https://luminischool.onrender.com/terms-and-conditions" />
+                <meta name="description" content="Consultez les conditions générales d’utilisation et de vente de LUMINI School : inscription, paiement, accès aux formations en présentiel et responsabilités."/>
+
+                <meta property="og:title" content="Conditions générales d’utilisation et de vente | LUMINI School - Plateforme de formation en informatique" />
+                <meta property="og:description" content="Consultez les conditions générales d’utilisation et de vente de LUMINI School : inscription, paiement, accès aux formations en présentiel et responsabilités." key="og:description"/>
+                <meta property="og:url" content="https://luminischool.onrender.com/terms-and-conditions" />
+
+                <meta name="twitter:title" content="Conditions générales d’utilisation et de vente | LUMINI School - Plateforme de formation en informatique" />
+                <meta name="twitter:description" content="Consultez les conditions générales d’utilisation et de vente de LUMINI School : inscription, paiement, accès aux formations en présentiel et responsabilités." />       
+            </Head>
+        </>
+    )
 }
