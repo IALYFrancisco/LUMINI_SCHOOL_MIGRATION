d25c297833b39258cbdf35250e676bea23d952c9
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-04-09 10:41:54 +0300

    ajout de script pour le GA4

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -5,6 +5,21 @@ export default function Document() {
     <Html lang="fr">
       <Head>
         <meta name="google-site-verification" content="prxvWUAcdDZhGnpWNHXTx8_N3BscnOiYra8fmQUGPrE" />
+	{/* Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-9S8XDP21DT"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-9S8XDP21DT', { page_path: window.location.pathname });
+            `,
+          }}
+        ></script>
       </Head>
       <body>
         <Main />
