065e66e0798319cafbeda1600a610949e687c2d8
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-04-15 07:38:45 +0300

    amélioration un peu du seo du page des formations

--- pages/formations.jsx
+++ pages/formations.jsx
@@ -51,7 +51,7 @@ export default function FormationsPage(){
             <Nav></Nav>
             <section className="formations-page">
                 <div className="head">
-                    <h2>Toute nos formations :</h2>
+                    <h1>Toute nos formations :</h1>
                     <p>Ci-dessous la liste de toute nos formations. Elles sont issues des branches existantes du secteur de l'informatique et ont été éléborées par nous-même afin de garantir leurs contenus ✨.</p>
                     <div className="actions">
                         <input type="text" name="formation" id="" value={prompt} onChange={(e)=>setPrompt(e.target.value)} placeholder="Rehcrecher des formations"/>
