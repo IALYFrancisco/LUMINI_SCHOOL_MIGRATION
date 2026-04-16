fb7287990fab88d0491b78aad9cd4eb04711fa8f
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-04-16 13:31:27 +0300

    dynamisation de la liste des transactions pour les simples utilisateurs

--- pages/dashboard/transactions.jsx
+++ pages/dashboard/transactions.jsx
@@ -129,24 +129,24 @@ export default function Transactions(){
                             <p>{transaction.paymentMode}</p>
                         </li>
                         <li  className="course-price-payed">
-                            { transaction.transactionState === "success" && <div className="badge yes">
+                            { transaction.mvolamodetransaction.status === "success" && <div className="badge yes">
                                 <p>validée</p>
                             </div> }
-                            { transaction.transactionState === "pending" && <div className="badge pending">
+                            { transaction.mvolamodetransaction.status === "pending" && <div className="badge pending">
                                 <p>en attente</p>
                             </div> }
-                            { transaction.transactionState === "failed" && <div className="badge no">
+                            { transaction.mvolamodetransaction.status === "failed" && <div className="badge no">
                                 <p>annulée</p>
                             </div> }
                         </li>
                         <li  className="begin-date">
-                            <p>{ new Date(transaction.transactionTime).toLocaleString("fr-FR") }</p>
+                            <p>{ new Date(transaction.mvolamodetransaction.transactionDate).toLocaleString("fr-FR") }</p>
                         </li>
                         <li  className="end-date">
                             <p>{ transaction.registration_id }</p>
                         </li>
                         <li  className="course-price">
-                            <p>{ transaction.transactionAmount } Ar</p>
+                            <p>{ transaction.formation.coursePrice } Ar</p>
                         </li>
                         <li className="registration-actions">
                             <ul className={ activePopUp === transaction._id ? 'pop-up show' : 'pop-up hide'}>
