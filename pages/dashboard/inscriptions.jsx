3b598662c8e93e5904c4bd27c3b30c76049a1804
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-04-21 13:32:55 +0300

    développement du fonctionnalité de téléchargement en PDF des détails de transaction

--- pages/dashboard/inscriptions.jsx
+++ pages/dashboard/inscriptions.jsx
@@ -37,6 +37,13 @@ export default function Inscriptions(){
         }
     }, [])
 
+    const GetPDFRegistrationDetails = async (registration_id) =>{
+        try{
+            
+        }
+        catch{}
+    }
+
     return(
         <Dashboard>
             <>
