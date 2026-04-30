abcebbb875fab93081b971915456e71d4f089006
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-04-30 07:43:51 +0300

    correction du bug de l'import de toast from sonner

--- components/dashboard/articles/article-list.jsx
+++ components/dashboard/articles/article-list.jsx
@@ -5,7 +5,7 @@ import Link from "next/link"
 import { useRouter } from "next/router"
 import DOMPurify from "isomorphic-dompurify"
 import Image from "next/image"
-import toast from 'sonner'
+import { toast } from "sonner"
 
 export default function ArticlesList(){
 
