61289fe6315a1129c30d853981d65108c9c82ab0
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-01 05:18:38 +0300

    création de nouvelle fonction de refacteur de date

--- contexts/DateRefactoring.js
+++ contexts/DateRefactoring.js
@@ -5,3 +5,15 @@ export default function DateRefactoring(_date){
         date.getFullYear()+"-"+pad(date.getMonth()+1)+"-"+pad(date.getDate())+"T"+pad(date.getHours())+":"+pad(date.getMinutes())
     )  
 }
+
+export function FormatDateMG(date){
+    return new Intl.DateTimeFormat("fr-FR", {
+        timeZone: "Indian/Antananarivo",
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: false
+    }).format(new Date(date))
+}
