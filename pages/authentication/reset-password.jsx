100466123d69f5e0abd02851c20f33214fdf020c
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-15 12:08:11 +0300

    intégration encore de la page de réinitialisation de mot de passe

--- pages/authentication/reset-password.jsx
+++ pages/authentication/reset-password.jsx
@@ -1,3 +1,37 @@
+import Head from "next/head"
+import Image from "next/image"
+import { Nav } from "@/components/nav"
+import { useForm } from "react-hook-form"
+
 export default function ResetPassword(){
-    return(<h1>This is reset password page ;)</h1>)
+
+    const { register } = useForm()
+
+    return(
+        <>
+            <Head>
+                <title>Réinitialisation de mot de passe | LUMINI School - Plateforme de formation en informatique</title>
+            </Head>
+            <Nav/>
+            <section className="reset-password-form">
+                <h2>Réinitialisation de mot de passe</h2>
+                <p>Ceci est la page de rénitialisation de mot de passe, veulliez fournir un nouveau mot de passe puis confirmez-le ensuite.</p>
+                <form>
+                    <Image src="/images/fleur.png" alt="fleur" className="laptop-mouse" width={400} height={400} priority />
+                    <Image src="/images/coffee-laptop.png" alt="café et laptop" className="mouse" width={400} height={400} priority />
+                    <div className="element">
+                        <label htmlFor="newPassword">Nouveau mot de passe :</label>
+                        <input type="password" id="newPassword" placeholder="Ex: johndoe@example.com" { ...register('newPassword', { required: true }) } required />
+                    </div>
+                    <div className="element">
+                        <label htmlFor="confirmPassword">Confirmez le nouveau mot de passe :</label>
+                        <input type="password" id="confirmPassword" placeholder="Ex: johndoe@example.com" { ...register('confirmPassword', { required: true }) } required />
+                    </div>
+                    <div className="element">
+                        <button>Soumettre</button>
+                    </div>
+                </form>
+            </section>
+        </>
+    )
 }
