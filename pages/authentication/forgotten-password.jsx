7c1f24d4bd0a0a53bc6915a803a72d6a8f3db0a3
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-05-15 10:12:19 +0300

    création de fichier de style pour la page des mots de passe oubliés

--- pages/authentication/forgotten-password.jsx
+++ pages/authentication/forgotten-password.jsx
@@ -1,3 +1,11 @@
+import Head from "next/head"
+
 export default function ForgottenPassword(){
-    return(<h1>This is the forgotten password page ;)</h1>)
+    return(
+        <>
+            <Head>
+                <title>Mot de passe oublié | LUMINI School - Plateforme de formation en informatique</title>
+            </Head>
+        </>
+    )
 }
